6f06ab6ff82a5d406dbd41db1a57826785ca29c9
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2022-02-07 03:42:03 +0300

    Added: offset, fixed some events, new reset offset button

--- js/main.js
+++ js/main.js
@@ -4,6 +4,21 @@ const App = {
     width: 5,
     height: 5,
     scale: 1,
+    offset: {
+      x: 0,
+      y: 0,
+    },
+    mouse: {
+      isDown: false,
+      start: {
+        x: 0,
+        y: 0,
+      },
+      current: {
+        x: 0,
+        y: 0,
+      },
+    },
   },
   settings: {
     scaleStep: 0.8,
@@ -19,6 +34,36 @@ App.OnScaleChanged = (e) => {
 App.OnScaleUp = () => App.SetScale(App.state.scale / App.settings.scaleStep);
 App.OnScaleDown = () => App.SetScale(App.state.scale * App.settings.scaleStep);
 App.OnScaleReset = () => App.SetScale(1);
+
+App.OnMovingDown = (e) => {
+  const { mouse } = App.state;
+  mouse.start.x = e.screenX;
+  mouse.start.y = e.screenY;
+  mouse.current.x = mouse.start.x;
+  mouse.current.y = mouse.start.y;
+  mouse.isDown = true;
+};
+App.OnMovingMove = (e) => {
+  const { mouse, offset } = App.state;
+  if (!mouse.isDown) return;
+  mouse.current.x = e.screenX;
+  mouse.current.y = e.screenY;
+  const { x, y } = mouse.start;
+  const dx = mouse.current.x - x;
+  const dy = mouse.current.y - y;
+  App.Var('offset-x', `${offset.x + dx}px`);
+  App.Var('offset-y', `${offset.y + dy}px`);
+};
+App.OnMovingUp = () => {
+  const { mouse, offset } = App.state;
+  if (!mouse.isDown) return;
+  mouse.isDown = false;
+  App.SetOffset(
+    offset.x + mouse.current.x - mouse.start.x,
+    offset.y + mouse.current.y - mouse.start.y,
+  );
+};
+App.OnMovingReset = () => App.SetOffset(0, 0);
 
 /* Methods */
 App.GetElements = () => {
@@ -37,7 +82,13 @@ App.PrepareElements = () => {
   App.elements.scaleUpButton.addEventListener('click', App.OnScaleUp);
   App.elements.scaleDownButton.addEventListener('click', App.OnScaleDown);
   App.elements.scaleResetButton.addEventListener('click', App.OnScaleReset);
-  App.elements.mainObject.addEventListener('mousewheel', App.OnScaleWheel, true);
+
+  App.elements.mainObject.addEventListener('wheel', App.OnScaleWheel, true);
+  App.elements.mainObject.addEventListener('mousedown', App.OnMovingDown, true);
+  window.addEventListener('mouseup', App.OnMovingUp, true);
+  App.elements.mainObject.addEventListener('mousemove', App.OnMovingMove, true);
+  App.elements.offsetResetButton.addEventListener('click', App.OnMovingReset);
+
   App.SetScale(App.state.scale);
 };
 App.SetScale = (value) => {
@@ -47,6 +98,11 @@ App.SetScale = (value) => {
   App.Var('scale', value);
 };
 App.SetOffset = (x, y) => {
+  console.log({x,y});
+  x = Number.isNaN(+x) ? App.state.offset.x : x;
+  y = Number.isNaN(+y) ? App.state.offset.y : y;
+  App.state.offset.x = x;
+  App.state.offset.y = y;
   App.Var('offset-x', `${x}px`);
   App.Var('offset-y', `${y}px`);
 };
