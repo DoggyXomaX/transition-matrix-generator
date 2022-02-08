82358bb186194366478cc9fd87af4926fde10b14
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2022-02-08 12:10:16 +0300

    Modified: added minify option in concat and move to utils folder

---
+++ utils/concat.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const scripts_path = '../js';
+const output_file = '../build/main.js';
+const output_minified_file = '../build/main.js';
+const files = [];
+
+console.log(`Reading content from ${scripts_path}`);
+fs.readdirSync(scripts_path, {}).forEach((filename) => {
+  console.log(`\t${filename}`);
+  files.push(fs.readFileSync(`${scripts_path}/${filename}`, { encoding: 'utf8' }));
+});
+console.log('[ OK ]');
+
+console.log(`Writing content to file ${output_file}`);
+fs.writeFileSync(output_file, files.join('\n'), { encoding: 'utf8' });
+console.log('[ OK ]');
+
+if (process.argv[2] !== 'minify') return;
+
+const minifier = require('node-minify');
+console.log(`Minify from ${output_file} to ${output_minified_file}`);
+minifier.minify({
+  compressor: 'gcc',
+  input: output_file,
+  output: output_minified_file,
+  callback: function(err) {
+    if (!err) {
+      console.log('[ OK ]');
+    } else {
+      console.error(err);
+    }
+  }
+});
