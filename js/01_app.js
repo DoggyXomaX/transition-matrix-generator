73bc3ba531255e8cd76436892b5cc07bf54f54c4
DoggyXomaX <l_l_l_l-l_l_l_l@mail.ru>
2022-02-08 11:54:23 +0300

    Fixed: app code 4 tabs -> 2 tabs

--- js/01_app.js
+++ js/01_app.js
@@ -1,20 +1,20 @@
 const App = {
-    elements: null,
-    state: {
-      offset: { x: 0, y: 0 },
-      mouse: {
-        isDown: false,
-        start: { x: 0, y: 0 },
-        current: { x: 0, y: 0 },
-      },
-      grid: [],
+  elements: null,
+  state: {
+    offset: { x: 0, y: 0 },
+    mouse: {
+      isDown: false,
+      start: { x: 0, y: 0 },
+      current: { x: 0, y: 0 },
     },
-    settings: {
-      countX: 5,
-      countY: 5,
-      scale: 1,
-      scaleStep: 0.8,
-      gridElementSize: 30,
-    },
-  };
+    grid: [],
+  },
+  settings: {
+    countX: 5,
+    countY: 5,
+    scale: 1,
+    scaleStep: 0.8,
+    gridElementSize: 30,
+  },
+};
   
